3420e2c3a01ad2de48f739523ff1e0c40b42d9d6
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 16:35:00 +0700

    TUGAS 10 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas10.js
@@ -0,0 +1,54 @@
+// var a = 0;
+// var b = 1;
+// var c = 1;
+
+// for ( var i = 0; i < 30; i++){
+// c = a + b;
+// a = b;
+// b = c;
+// console.log(i)
+
+// }
+
+
+
+
+var x=1
+   for (var a = 1; a <= 20; a++) {
+     console.log(a+x)
+   }
+
+
+
+
+// var x= 1
+//    for (var a = 1; a <= 20; a++) {
+//      deret = a+x;
+//    }
+
+//    for (var b = 1; b <= 20; b++) {
+//    	console.log((b + deret))
+// }
+
+
+
+
+
+// var j = 0
+//    for ( var a = 1; a <= 30; a++){
+//      j = j + 1;
+//      console.log(a+" ")
+//    }
+
+
+
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 		int d = 1;
+//     	for(int i=1;i<50;i = d+i){
+//     		System.out.println(i);
+//     		d++;
+//     	}
+// 	}
+// }
